c1613d330cdddfbeac3c7f54b0bd8a294e4f29d6
Gahade <angrygahade@gmail.com>
2023-12-03 20:52:16 +0100

    Python script for finding files

--- main.py
+++ main.py
@@ -1,49 +1,38 @@
 """
 Завдання:
-Вам дано великий список цілих чисел. Ваше завдання полягає в тому,
-щоб розподілити цей список на дві приблизно рівні частини і
-використовувати два окремі процеси для підрахунку кількості парних чисел в кожній з цих частин.
+Вам потрібно створити Python скрипт, який використовує многопоточність для пошуку файлів з конкретним розширенням
+(наприклад, .txt або .jpg) в різних папках на вашому комп'ютері.
+
 Вимоги:
-1. Використовувати модуль `multiprocessing` для створення процесів.
-2. Результати обох процесів повинні бути підсумовані і виведені.
+1. Скрипт повинен приймати розширення файлу та список директорій для пошуку як вхідні параметри.
+2. Скрипт повинен використовувати багатопотоковість для одночасного пошуку в різних директоріях.
+3. Знайдені файли повинні виводитися на екран з інформацією про те, в якій папці вони були знайдені.
 """
-from multiprocessing import Process, Value, Array, Lock
+
+import os
+from concurrent.futures import ThreadPoolExecutor
 
 
-def count_even_numbers(numbers, result, lock):
-    count = 0
-    for num in numbers:
-        if num % 2 == 0:
-            count += 1
+def search_files(extension, directory):
+    found_files = []
+    for root, dirs, files in os.walk(directory):
+        for file in files:
+            if file.endswith(extension):
+                found_files.append(os.path.join(root, file))
+    return found_files
 
-    with lock:
-        result.value += count
+
+def main(extension, directories):
+    with ThreadPoolExecutor() as executor:
+        results = list(executor.map(search_files, [extension] * len(directories), directories))
+    for directory, files in zip(directories, results):
+        print(f"Знайдено файли в папці {directory}:")
+        for file in files:
+            print(file)
 
 
 if __name__ == "__main__":
-    numbers_list = list(range(1, 251))
-    half_of_list = len(numbers_list) // 2
+    target_extension = input("Введіть розширення файлів (наприклад, .txt): ")
+    target_directories = input("Введіть список директорій (розділіть їх комами): ").split(',')
 
-    first_half = numbers_list[:half_of_list]
-    second_half = numbers_list[half_of_list:]
-
-    result_first_half = Value('i', 0)
-    result_second_half = Value('i', 0)
-    lock = Lock()
-
-    process_first_half = Process(target=count_even_numbers, args=(first_half, result_first_half, lock))
-    process_second_half = Process(target=count_even_numbers, args=(second_half, result_second_half, lock))
-
-    process_first_half.start()
-    process_second_half.start()
-
-    process_first_half.join()
-    process_second_half.join()
-
-    total_even_numbers = result_first_half.value + result_second_half.value
-
-    print("Кількість парних чисел в першій половині:", result_first_half.value)
-    print("Кількість парних чисел в другій половині:", result_second_half.value)
-    print("Загальна кількість парних чисел:", total_even_numbers)
-
-
+    main(target_extension, target_directories)
